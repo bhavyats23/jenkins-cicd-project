00749ff0ec07643c6591bb50f55b29013ed91ecd
bhavyats23 <bhavyats23@email.com>
2026-03-21 12:04:28 +0000

    Initial commit: Jenkins CICD project

---
+++ src/test/java/com/demo/AppTest.java
@@ -0,0 +1,9 @@
+package com.demo;
+import org.junit.Test;
+import static org.junit.Assert.*;
+public class AppTest {
+    @Test
+    public void testGreet() {
+        assertEquals("Hello, Jenkins CI/CD!", App.greet("Jenkins CI/CD"));
+    }
+}
