da3442c594613cb3442e6858c164a647fb9c8645
bhavyats23 <bhavyats23@gmail.com>
2026-07-08 11:56:42 +0530

    Update AppTest.java

--- src/test/java/com/demo/AppTest.java
+++ src/test/java/com/demo/AppTest.java
@@ -4,6 +4,6 @@ import static org.junit.Assert.*;
 public class AppTest {
     @Test
     public void testGreet() {
-        assertEquals("Hello, Jenkins CI/CD!", App.greet("Jenkins CI/CD"));
+        assertEquals("Hello and welcome, Jenkins CI/CD!", App.greet("Jenkins CI/CD"));
     }
 }
